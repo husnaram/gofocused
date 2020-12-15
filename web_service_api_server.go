e07e2f51f21e7c93eb549a49409bda5075d4e760
Mandasialearn <mandasialearn@gmail.com>
2020-12-15 21:19:31 +0700

    Update some line

--- web_service_api_server.go
+++ web_service_api_server.go
@@ -2,34 +2,71 @@ package gofocused
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 type student struct {
-	ID    string
-	Name  string
-	Grade int
+	ID    string `json:"id,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Grade int    `json:"grade,omitempty"`
 }
 
-var data = []student{
-	student{"E001", "ethan", 21},
-	student{"W001", "wick", 22},
-	student{"B001", "bourne", 23},
-	student{"B002", "bond", 23},
+var Data = []student{
+	{"E001", "ethan", 21},
+	{"W001", "wick", 22},
+	{"B001", "bourne", 23},
+	{"B002", "bond", 23},
+}
+
+func RunWebServices() {
+	http.HandleFunc("/users", users)
+	http.HandleFunc("/user", user)
+
+	fmt.Println("Starting web server at http://localhost:9090")
+	http.ListenAndServe(":9090", nil)
 }
 
 func users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	if r.Method == "POST" {
-		var result, err = json.Marshal(data)
-
+		var result, err = json.Marshal(Data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.Write(result)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		var id = r.FormValue("id")
+		var result []byte
+		var err error
+
+		for _, each := range Data {
+			if each.ID == id {
+				result, err = json.Marshal(each)
+
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				w.Write(result)
+				return
+			}
+		}
+
+		http.Error(w, "User not found", http.StatusBadRequest)
 		return
 	}
 
